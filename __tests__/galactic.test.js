7ed967fdb451746d5b6faf3332f265f65af0bdff
Eva Kemp <eva.j.kemp@gmail.com>
2023-06-23 11:19:40 -0700

    defining constructor GalacticAge passed test

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -2,13 +2,14 @@ import GalacticAge from "./../src/galactic.js";
 
 describe("GalacticAge", () => {
   let age;
+  let galacticAge;
 
   beforeEach(() => {
     age = 0;
     galacticAge = new GalacticAge(age);
   });
-});
 
-test("should define GalaticAge constructor", () => {
-  expect(galacticAge.age).toBe(age);
+  test("should define GalaticAge constructor", () => {
+    expect(galacticAge.age).toBe(age);
+  });
 });
--- src/galactic.js
+++ src/galactic.js
@@ -3,7 +3,7 @@ Venus years. (A Venus year is .62 Earth years.)
 Mars years. (A Mars year is 1.88 Earth years.)
 Jupiter years. (A Jupiter year is 11.86 Earth years.) */
 
-class GalacticAge {
+export default class GalacticAge {
   constructor(age) {
     this.age = age;
   }
